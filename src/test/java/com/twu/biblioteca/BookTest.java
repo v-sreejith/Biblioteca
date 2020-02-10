ebc9499a6e801687e63aa8544320dcc372fb38a2
Sreejith V <sreejithv13055@gmail.com>
2020-02-10 19:50:05 +0530

    [Sreejith] #1.2 Refactor. added tests for getters in book class

--- src/test/java/com/twu/biblioteca/BookTest.java
+++ src/test/java/com/twu/biblioteca/BookTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
@@ -15,5 +14,26 @@ class BookTest {
         Book bookTwo = new Book("ABC", "meee", 2000);
 
         assertThat(bookOne, is(equalTo(bookTwo)));
+    }
+
+    @Test
+    public void shouldReturnNameOfBook() {
+        Book bookOne = new Book("ABC", "meee", 2000);
+
+        assertThat(bookOne.getName(), is(equalTo("ABC")));
+    }
+
+    @Test
+    public void shouldReturnAuthorOfBook() {
+        Book bookOne = new Book("ABC", "meee", 2000);
+
+        assertThat(bookOne.getAuthor(), is(equalTo("meee")));
+    }
+
+    @Test
+    public void shouldReturnYearOfPublicationOfBook() {
+        Book bookOne = new Book("ABC", "meee", 2000);
+
+        assertThat(bookOne.getYear(), is(equalTo(2000)));
     }
 }
