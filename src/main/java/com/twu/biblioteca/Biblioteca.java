4800c1616272d1b191cd55defda746bfccb863e5
Sreejith V <sreejithv13055@gmail.com>
2020-02-10 18:42:58 +0530

    [Sreejith] #1.2 Refactor. Return list of library books from biblioteca app

--- src/main/java/com/twu/biblioteca/Biblioteca.java
+++ src/main/java/com/twu/biblioteca/Biblioteca.java
@@ -1,11 +1,18 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
 //Job: Manage library
 public class Biblioteca {
-
     public static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+    Library library = new Library();
 
     public String getWelcomeMessage() {
         return WELCOME_MESSAGE;
+    }
+
+    public List<Book> getLibraryBooks() {
+        return library.getAvailableBooks();
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaTest.java
@@ -2,6 +2,8 @@ package com.twu.biblioteca;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -13,5 +15,16 @@ class BibliotecaTest {
         Biblioteca biblioteca = new Biblioteca();
 
         assertThat(biblioteca.getWelcomeMessage(), is(equalTo(("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"))));
+    }
+
+    @Test
+    public void shouldReturnListOfBooksInLibrary() {
+        Biblioteca biblioteca = new Biblioteca();
+        Book bookOne = new Book("Wings of Fire", "A P J Abdul Kalam", 2001);
+        Book bookTwo = new Book("Kite Runner", "Khaled Hosseini", 2003);
+        Book bookThree = new Book("Hunger Games", "Suzzane", 2009);
+        List<Book> books = List.of(bookOne, bookTwo, bookThree);
+
+        assertThat(biblioteca.getLibraryBooks(), is(equalTo(books)));
     }
 }
