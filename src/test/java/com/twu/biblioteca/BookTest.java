cfc29cdec8f4455cd959d61189106a3bb10b252b
Sreejith V <sreejithv13055@gmail.com>
2020-02-10 19:46:39 +0530

    [Sreejith] #1.2 Refactor. added test for book class to equate two books

---
+++ src/test/java/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    public void shouldEquateTwoBooks() {
+        Book bookOne = new Book("ABC", "meee", 2000);
+        Book bookTwo = new Book("ABC", "meee", 2000);
+
+        assertThat(bookOne, is(equalTo(bookTwo)));
+    }
+}
